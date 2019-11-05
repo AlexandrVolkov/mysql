6af2630be64d1a261e860b74a9715392a02ceab6
AlexandrVolkov <alexandr25011987@gmail.com>
2019-11-05 19:40:38 +0200

    task9

--- 9/task3.sql
+++ 9/task3.sql
@@ -1,12 +1,34 @@
 -- 1
 drop function if exists my_new_function;
 DELIMITER $$
-create function my_new_function
+create function my_new_function()
 returns varchar(255)
 begin
+    declare text varchar(255);
 	declare cur_hour int;
-	set cur_hour = hour(now())
-	if ()
-end
+	set cur_hour = hour(now());
+	if cur_hour < 6 then set text="Good night";
+	elseif cur_hour < 12 then set text="Good morning";
+	elseif cur_hour < 18 then set text="Good afternoon";
+	else set text="good evening";
+	end if;
+	return (text);
+end$$
+DELIMITER ;
 
---С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+select my_new_function();
+
+-- 2
+
+drop trigger if exists beforeinsertname;
+DELIMITER $$
+CREATE TRIGGER beforeinsertname
+AFTER INSERT
+ON products FOR EACH row
+begin
+	if (new.description  is null and new.name is null) then
+		signal sqlstate '45000' set message_text = 'Both fields name and description can not be empty';
+	end if;
+end$$
+DELIMITER ;
+
